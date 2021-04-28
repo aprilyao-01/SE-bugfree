dace816304c3b20226aadcb8a84180bee5b3170c
Jing Zhang <scyjz12@nottingham.edu.cn>
2021-04-28 21:01:48 +0800

    Fix the BoCCategory.java code

--- src/CW3/src/cw3/BoCCategory.java
+++ src/CW3/src/cw3/BoCCategory.java
@@ -14,9 +14,32 @@ public class BoCCategory {
 	}
 
 	public BoCCategory(String newTitle) {
-		CategoryName = newTitle;
-		CategoryBudget = new BigDecimal("0.00");
-		CategorySpend = new BigDecimal("0.00");
+		/*
+		 * Modified by : JingZHANG
+		 * Last modified time: 21/4/28 20:22
+		 * Edit: 1) Use do while to to avoid user input illegal category name
+		 *       2) If user input illegal category name, should enter again
+		 */
+		// 1 means bigger, -1 means smaller, 0 means same
+		int n = 0;
+		do{
+			if (newTitle == null) {
+				System.out.println("Title could not be NULL, please enter again:");
+				Scanner in = new Scanner(System.in);
+				newTitle = in.nextLine();
+				continue;
+			}else if (newTitle.length() > 15){
+				System.out.println("Title could not be more than 15 characters, please enter again:");
+				Scanner in = new Scanner(System.in);
+				newTitle = in.nextLine();
+				continue;
+			}else {
+				CategoryName = newTitle;
+				CategoryBudget = new BigDecimal("0.00");
+				CategorySpend = new BigDecimal("0.00");
+				n += 1;
+			}
+		} while (n == 0);
 	}
 
 	public String CategoryName() {
@@ -30,13 +53,14 @@ public class BoCCategory {
 	public BigDecimal CategorySpend() {
 		return CategorySpend;
 	}
-	/*
-	 * Modified by: Siyu Yao
-	 * Last modified time: 21/04/28 19:34
-	 * Edit: 1)Add the do-while loop to make sure that the category name cannot be Unknown/blank/more than 15 char
-	 */
+	
 	
 	public void setCategoryName(String newName) {
+		/*
+		* Modified by: Siyu Yao
+		* Last modified time: 21/04/28 19:34
+		* Edit: 1)Add the do-while loop to make sure that the category name cannot be Unknown/blank/more than 15 char
+		*/
 		int n=0;
 		do {
 			if(newName.equalsIgnoreCase("Unknown") || newName.length() > 15 || newName.isBlank()) {
@@ -52,6 +76,25 @@ public class BoCCategory {
 	}
 
 	public void setCategoryBudget(BigDecimal newValue) {
+		/*
+		 * Modified by : JingZHANG
+		 * Last modified time: 21/4/28 20:01
+		 * Edit: 1) Use do while to to avoid user input illegal budget value
+		 *       2) If user input illegal budget value, should enter again
+		 */
+		// 1 means bigger, -1 means smaller, 0 means same
+		int n = 0;
+		do{
+			if (newValue.compareTo(new BigDecimal("0.00")) != 1) {
+				System.out.println("Illegal budget, please enter again:");
+				Scanner in = new Scanner(System.in);
+				newValue = in.nextBigDecimal();
+				continue;
+			}else {
+				CategoryBudget = newValue;
+				n += 1;
+			}
+		} while (n == 0);
 		// 1 means bigger, -1 means smaller, 0 means same
 		if (newValue.compareTo(new BigDecimal("0.00")) == 1) {
 			CategoryBudget = newValue;
@@ -75,14 +118,15 @@ public class BoCCategory {
 		return remainingBudget;
 	}
 
-	/*
-	 * Modified by: Siyu Yao
-	 * Last modified time: 21/04/28 18:55
-	 * Edit: 1) Delete the 'Est' in return format, cause this information is useless
-	 * 		 2) Add the format when spent is larger than budget and return overspent format
-	 */
+	
 	@Override
 	public String toString() {
+		/*
+		* Modified by: Siyu Yao
+		* Last modified time: 21/04/28 18:55
+		* Edit: 1) Delete the 'Est' in return format, cause this information is useless
+		* 		 2) Add the format when spent is larger than budget and return overspent format
+		*/
 		if(CategoryBudget.compareTo(CategorySpend)!=-1) 		// if budget <= spend return format should be 'Overspent'
 		{
 			return CategoryName + "(¥" + CategoryBudget.toPlainString() + ") - ¥" + CategorySpend.toPlainString()
